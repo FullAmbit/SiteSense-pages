691c8327841e54c4c28fccb80326591a44ca5329
Zach Bloomquist <zbloomquist@fullambit.net>
2012-11-18 12:28:25 -0500

    Removing empty unique settings function

--- modules/pages/pages.module.php
+++ modules/pages/pages.module.php
@@ -22,9 +22,6 @@
 * @copyright  Copyright (c) 2011 Full Ambit Media, LLC (http://www.fullambit.com)
 * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 */
-function pages_getUniqueSettings($data,$db) {
-}
-
 function pages_buildContent($data,$db) {
 	$statement = $db->prepare('getPageByShortNameAndParent', 'pages');
 	$current = array('id' => 0); //pseudo-page, root node.
